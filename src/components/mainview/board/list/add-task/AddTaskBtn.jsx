8dcd6582f995102bacbe7a8503c618e792653d1b
devdreamercode <devdreamercode@gmail.com>
2024-09-09 16:36:31 +0100

    updated the modal to close correctly

--- src/components/mainview/board/list/add-task/AddTaskBtn.jsx
+++ src/components/mainview/board/list/add-task/AddTaskBtn.jsx
@@ -1,19 +1,25 @@
+import { useState } from "react";
 import Modal from "../../../../ui-components/Modal";
 import AddCard from "../card/AddCard";
 
 import { PiPlusBold } from "react-icons/pi";
 
 export default function AddTaskBtn({list}) {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <Modal
-      trigger={
-        <button className="flex w-[250px] items-center justify-center rounded-lg border bg-white p-4 font-medium text-[#365dff] md:w-[300px] dark:border-drkbrd dark:bg-drkbg dark:text-drkcol">
-          <PiPlusBold />
-          <p className="text-sm md:text-base">&nbsp;Add New Task</p>
-        </button>
-      }
-    >
-      <AddCard list={list} />
-    </Modal>
+    <>
+      <button
+        className="flex w-[250px] items-center justify-center rounded-lg border bg-white p-4 font-medium text-[#365dff] md:w-[300px] dark:border-drkbrd dark:bg-drkbg dark:text-drkcol"
+        onClick={() => setIsOpen(true)}
+      >
+        <PiPlusBold />
+        <p className="text-sm md:text-base">&nbsp;Add New Task</p>
+      </button>
+      {isOpen ? (
+        <Modal closeModal={() => setIsOpen(false)}>
+          <AddCard list={list} onClose={() => setIsOpen(false)} />
+        </Modal>
+      ) : null}
+    </>
   );
 }
--- src/components/mainview/mainview-header/MainViewMembers.jsx
+++ src/components/mainview/mainview-header/MainViewMembers.jsx
@@ -24,12 +24,11 @@ export default function MainViewMembers() {
   );
 
   const members = board ? board.members : [];
-  //const membersLength = board ? board.members.length : null;
-
-  //const members = useSelector((state) => state.members); 
+  const membersLength = board ? board.members.length : null;
   
   const [memberName, setMemberName] = useState("");
   const [memberImg, setMemberImg] = useState(null);
+  const [isOpen, setIsOpen] = useState(false);
   const memberImgUpload = useRef(null);
   const memberImgFrame = useRef(null);
   const formRef = useRef(null);
@@ -55,23 +54,16 @@ export default function MainViewMembers() {
     formRef.current.reset();
     setMemberImg(null);
     setMemberName("");
+    setIsOpen(false);
   }
 
   function handleSubmit(e) {
     e.preventDefault();
     const memberInfo = {
-      id: 100,
+      id: membersLength + 1,
       name: memberName,
       img: memberImg,
     };
-
-    // dispatch(addMember(memberInfo));
-    // dispatch(
-    //   addBoardMember({ boardId: parseInt(boardId), member: memberInfo }),
-    // );
-
-    // console.log("Updated Members:", members);
-    // console.log("new member:", memberInfo);
     
     dispatch(addMember(memberInfo));
     dispatch(addBoardMember({ id: parseInt(boardId), newMember: memberInfo }));
@@ -86,123 +78,127 @@ export default function MainViewMembers() {
     <div className="col-start-2 col-end-3 row-start-1 row-end-2 flex items-center gap-3 justify-self-end">
       <div className="hidden md:block">
         <div className="relative flex w-fit">
-            <MemberCircles imgs={members} size={35} side={`right`}/>
+          <MemberCircles imgs={members} size={35} side={`right`} />
         </div>
       </div>
       <div>
-        <Modal
-          trigger={
-            <button className="btn-transition flex cursor-pointer items-center whitespace-nowrap rounded-md bg-[#365dff] p-3 text-sm font-normal text-white">
-              <div className="hidden items-center md:flex">
-                <PiPlusBold />
-                <p>&nbsp;Add Member</p>
-              </div>
-              <PiUserPlusBold className="text-xl md:hidden" />
-            </button>
-          }
-        >
-          <form
-            ref={formRef}
-            className="flex h-[100%] flex-col justify-between gap-5"
-            onSubmit={handleSubmit}
+        <>
+          <button
+            className="btn-transition flex cursor-pointer items-center whitespace-nowrap rounded-md bg-[#365dff] p-3 text-sm font-normal text-white"
+            onClick={() => setIsOpen(true)}
           >
-            <div className="space-y-12">
-              <div className="">
-                <div className="flex gap-4">
-                  <h2 className="text-lg font-semibold leading-7">
-                    Add a new member
-                  </h2>
-                </div>
-                <div
-                  className="mt-4 flex basis-1/3 cursor-pointer flex-col items-center justify-center gap-2"
-                  onClick={() => memberImgUpload.current.click()}
-                >
-                  <div
-                    ref={memberImgFrame}
-                    className={`borderd-[1px] flex h-[100px] w-[100px] items-center justify-center rounded-full ${!memberImg ? "border-[#365dff]" : null} bg-blue-100`}
-                  >
-                    {memberImg ? (
-                      <img
-                        src={memberImg}
-                        alt="Profile Picture"
-                        className="h-full w-full rounded-full object-cover"
-                      />
-                    ) : (
-                      <PiCameraBold className="text-2xl text-blue-950" />
-                    )}
-                  </div>
-                </div>
-                <p className="mt-2 text-center font-semibold dark:text-white">
-                  {memberName}
-                </p>
+            <div className="hidden items-center md:flex">
+              <PiPlusBold />
+              <p>&nbsp;Add Member</p>
+            </div>
+            <PiUserPlusBold className="text-xl md:hidden" />
+          </button>
+          {isOpen ? (
+            <Modal closeModal={() => setIsOpen(false)}>
+              <form
+                ref={formRef}
+                className="flex h-[100%] flex-col justify-between gap-5"
+                onSubmit={handleSubmit}
+              >
+                <div className="space-y-12">
+                  <div className="">
+                    <div className="flex gap-4">
+                      <h2 className="text-lg font-semibold leading-7">
+                        Add a new member
+                      </h2>
+                    </div>
+                    <div
+                      className="mt-4 flex basis-1/3 cursor-pointer flex-col items-center justify-center gap-2"
+                      onClick={() => memberImgUpload.current.click()}
+                    >
+                      <div
+                        ref={memberImgFrame}
+                        className={`borderd-[1px] flex h-[100px] w-[100px] items-center justify-center rounded-full ${!memberImg ? "border-[#365dff]" : null} bg-blue-100`}
+                      >
+                        {memberImg ? (
+                          <img
+                            src={memberImg}
+                            alt="Profile Picture"
+                            className="h-full w-full rounded-full object-cover"
+                          />
+                        ) : (
+                          <PiCameraBold className="text-2xl text-blue-950" />
+                        )}
+                      </div>
+                    </div>
+                    <p className="mt-2 text-center font-semibold dark:text-white">
+                      {memberName}
+                    </p>
 
-                <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                  <div className="sm:col-span-full">
-                    <div className="flex items-center gap-2">
-                      <PiUserCirclePlusBold className="text-xl" />
-                      <label
-                        htmlFor="member-name"
-                        className="block text-sm font-medium leading-6"
-                      >
-                        Full name
-                      </label>
+                    <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                      <div className="sm:col-span-full">
+                        <div className="flex items-center gap-2">
+                          <PiUserCirclePlusBold className="text-xl" />
+                          <label
+                            htmlFor="member-name"
+                            className="block text-sm font-medium leading-6"
+                          >
+                            Full name
+                          </label>
+                        </div>
+                        <div className="mt-2">
+                          <input
+                            onChange={handleMemberName}
+                            id="member-name"
+                            name="member-name"
+                            type="text"
+                            placeholder="John Smith..."
+                            value={memberName}
+                            className="block w-full rounded-md border-0 py-1.5 text-sm shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-500 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:leading-6 dark:text-drkbg"
+                          />
+                        </div>
+                      </div>
                     </div>
-                    <div className="mt-2">
-                      <input
-                        onChange={handleMemberName}
-                        id="member-name"
-                        name="member-name"
-                        type="text"
-                        placeholder="John Smith..."
-                        value={memberName}
-                        className="block w-full rounded-md border-0 py-1.5 text-sm shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-500 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:leading-6 dark:text-drkbg"
-                      />
+                    <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                      <div className="sm:col-span-full">
+                        <div className="flex items-center gap-2">
+                          <PiUserFocusBold className="text-xl" />
+                          <label
+                            htmlFor="member-img"
+                            className="block text-sm font-medium leading-6"
+                          >
+                            Profile pic
+                          </label>
+                        </div>
+                        <div className="mt-2 w-[50px]">
+                          <input
+                            type="file"
+                            name="fileInput"
+                            id="fileInput"
+                            ref={memberImgUpload}
+                            onChange={handleImgChange}
+                            className="text-sm"
+                          />
+                        </div>
+                      </div>
                     </div>
                   </div>
                 </div>
-                <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                  <div className="sm:col-span-full">
-                    <div className="flex items-center gap-2">
-                      <PiUserFocusBold className="text-xl" />
-                      <label
-                        htmlFor="member-img"
-                        className="block text-sm font-medium leading-6"
-                      >
-                        Profile pic
-                      </label>
-                    </div>
-                    <div className="mt-2 w-[50px]">
-                      <input
-                        type="file"
-                        name="fileInput"
-                        id="fileInput"
-                        ref={memberImgUpload}
-                        onChange={handleImgChange}
-                        className="text-sm"
-                      />
-                    </div>
-                  </div>
+                <div className="mt-5 flex items-center justify-end gap-6">
+                  <button
+                    type="button"
+                    className="text-sm font-semibold"
+                    onClick={handleClickCancelBtn}
+                  >
+                    Cancel
+                  </button>
+                  <button
+                    type="submit"
+                    disabled={!memberName || !memberImg}
+                    className={`rounded-md px-3 py-2 text-sm font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 ${memberName && memberImg ? "bg-[#365dff] text-white" : "bg-gray-400 text-white"}`}
+                  >
+                    Add Member
+                  </button>
                 </div>
-              </div>
-            </div>
-            <div className="mt-5 flex items-center justify-end gap-6">
-              <button
-                type="button"
-                className="text-sm font-semibold"
-                onClick={handleClickCancelBtn}
-              >
-                Cancel
-              </button>
-              <button
-                type="submit"
-                disabled={!memberName || !memberImg}
-                className={`rounded-md px-3 py-2 text-sm font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 ${memberName && memberImg ? "bg-[#365dff] text-white" : "bg-gray-400 text-white"}`}
-              >
-                Add Member
-              </button>
-            </div>
-          </form>
-        </Modal>
+              </form>
+            </Modal>
+          ) : null}
+        </>
       </div>
     </div>
   );
