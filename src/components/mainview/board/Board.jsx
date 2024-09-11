e6d4b1bf8160202550e8b9643edaa515a54c3cf7
devdreamercode <devdreamercode@gmail.com>
2024-09-11 10:09:19 +0100

    Sorted the scrolling issues

--- src/components/mainview/board/Board.jsx
+++ src/components/mainview/board/Board.jsx
@@ -11,7 +11,7 @@ export default function Board() {
   );
 
   return (
-    <div className="#181a1f fixed right-0 top-[230px] z-[-1] h-[calc(100%_-_230px)] w-full overflow-x-auto overflow-y-hidden border-l bg-[#f7f7f7] p-5 lg:w-[calc(100%_-_25vw)] 2xl:w-[calc(100%_-_20vw)] dark:border-drkbrd dark:bg-drkbg dark:text-drkcol">
+    <div id="board" className="#181a1f fixed right-0 top-[230px] z-[-1] h-[calc(100%_-_230px)] w-full overflow-x-auto overflow-y-auto border-l bg-[#f7f7f7] p-5 lg:w-[calc(100%_-_25vw)] 2xl:w-[calc(100%_-_20vw)] dark:border-drkbrd dark:bg-drkbg dark:text-drkcol">
       <ol className="no-scrollbar grid h-[100%] w-auto grid-flow-col gap-4 overflow-x-auto overflow-y-hidden md:auto-cols-[minmax(350px,300px)]">
         {board && board.lists.map((list) => <List key={list.id} list={list} />)}
       </ol>
