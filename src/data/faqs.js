a5923272eb2936a00eef660ea42b6db4dfc36c59
devdreamercode <devdreamercode@gmail.com>
2024-10-01 13:50:59 +0100

    added new faq item to data

--- src/data/faqs.js
+++ src/data/faqs.js
@@ -3,21 +3,20 @@ const faqs = [
     id: 1,
     question: "What technologies and tools did you use to build this project?",
     answer:
-      "This project was built using a modern web stack including React JS for the front end, Redux for global state management, React Router Dom for navigation, and Tailwind CSS for styling. I also used tools like Git/GitHub for version control, React Icons, uuid for generating unique id's and React Dnd-Kit for the drag n drop feature for the cards.",
+      "This project was built from scratch using a modern web stack including React JS for the front end, Redux for global state management, React Router Dom for navigation, and Tailwind CSS for styling. I also used tools like Git/GitHub for version control, React Icons, uuid for generating unique id's and React Dnd-Kit for the drag n drop feature for the cards.",
     icons: [
-      {id: 1, src: "/src/assets/React-icon.png", title: "React JS"},
-      {id: 2, src: "/src/assets/redux-icon.webp", title: "Redux"},
-      {id: 3, src: "/src/assets/react-router.svg", title: "React Router DOM"},
-      {id: 3, src: "/src/assets/tailwind-icon.png", title: "Tailwind CSS"},
-      {id: 3, src: "/src/assets/github-logo.png", title: "Git/Github"},
-      {id: 3, src: "/src/assets/react-icons-icon.svg", title: "React Icons"},
-      {id: 3, src: "/src/assets/dndkit-icon.png", title: "dnd kit"},
-    ]
+      { id: 1, src: "/src/assets/React-icon.png", title: "React JS" },
+      { id: 2, src: "/src/assets/redux-icon.webp", title: "Redux" },
+      { id: 3, src: "/src/assets/react-router.svg", title: "React Router DOM" },
+      { id: 3, src: "/src/assets/tailwind-icon.png", title: "Tailwind CSS" },
+      { id: 3, src: "/src/assets/github-logo.png", title: "Git/Github" },
+      { id: 3, src: "/src/assets/react-icons-icon.svg", title: "React Icons" },
+      { id: 3, src: "/src/assets/dndkit-icon.png", title: "dnd kit" },
+    ],
   },
   {
     id: 2,
-    question:
-      "What challenges did you face and how did you overcome them?",
+    question: "What challenges did you face and how did you overcome them?",
     answer:
       "One challenge was managing state across multiple components efficiently, I solved this by implementing Redux for global centralized state management, which reduced prop drilling and made the code more scalable. My choice between Redux and Reacts Context API came down to the scalability of my project, it made more sense to go for Redux due to the size of the project. I also leveraged Tailwind CSS for custom media queries.",
   },
@@ -36,10 +35,15 @@ const faqs = [
   },
   {
     id: 5,
-    question:
-      "How can the project be improved?",
+    question: "How can the project be improved?",
     answer:
       "There are may ways in which the project can be improved such as: (1) moving the data from using a file system to a back end with something like Mongo DB and Node JS to create a fully fledged full stack application, (2) Adding a filter button section on top of each board so users can filter cards by category, (3) Allowing for the creation of new lists. These are all things I could do to further improve the project.",
+  },
+  {
+    id: 6,
+    question: "Who designed the project?",
+    answer:
+      "I did not design the project, instead the vast majority of the design comes from Dribbble - https://dribbble.com/shots/21227113-Managemate-Task-Management-Dashboard with quite a few additional tweaks from myself.",
   },
 ];
 
--- src/routes/faqs/Faq.jsx
+++ src/routes/faqs/Faq.jsx
@@ -4,10 +4,18 @@ import FaqItem from "./FaqItem";
 
 export default function Faq() {
   return (
-    <MainContainer title={`Frequently asked questions`} subtitle={`Need some help? Here are some questions and answers all in one place `}>
-      <div className="flex flex-col gap-4 w-full md:p-2 xl:grid xl:grid-cols-2 xl:gap-y-10 xl:gap-x-5">
+    <MainContainer
+      title={`Frequently asked questions`}
+      subtitle={`Need some help? Here are some questions and answers all in one place `}
+    >
+      <div className="flex w-full flex-col gap-4 md:p-2 xl:grid xl:grid-cols-2 xl:gap-x-5 xl:gap-y-10">
         {faqs.map((f) => (
-          <FaqItem key={f.id} question={f.question} answer={f.answer} icons={f.icons} />
+          <FaqItem
+            key={f.id}
+            question={f.question}
+            answer={f.answer}
+            icons={f.icons}
+          />
         ))}
       </div>
     </MainContainer>
